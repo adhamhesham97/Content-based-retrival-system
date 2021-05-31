c2e0bbf75df4954481c41ff7d2738f3ba83b4c20
Reem-Mady <56718376+Reem-Mady@users.noreply.github.com>
2021-06-01 00:12:48 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -24,45 +24,46 @@ def browseFiles():
     ent1.insert(0,filename)
 def ShowImage():
     photo=ImageTk.PhotoImage(Image.open(ent1.get()))
-    l1=Label(window)
-    l1.config(image=photo)
-    l1.image=photo
-    l1.place(x=2,y=200)
-    l2=Label(window,text="Input Image",bg='#1f666b',height=1,width=10,bd=6,font='Helvetica 11 bold')
-    l2.place(x=20,y=160)
+    
+    IP_img=Label(window)
+    IP_img.config(image=photo)
+    IP_img.image=photo
+    IP_img.place(x=2,y=200)
+    
+    IPimg_L=Label(window,text="Input Image",bg='#1f666b',height=1,width=10,bd=6,font='Helvetica 11 bold')
+    IPimg_L.place(x=20,y=160)
+    
     photo1=ImageTk.PhotoImage(Image.open(image_path))
-    l3=Label(window)
-    l3.config(image=photo1)
-    l3.image=photo1
-    l3.place(x=470,y=200)
-    l4=Label(window,text="Retieved Image",bg='#1f666b',height=1,width=12,bd=6,font='Helvetica 11 bold')
-    l4.place(x=520,y=160)
+    
+    R_img=Label(window)
+    R_img.config(image=photo1)
+    R_img.image=photo1
+    R_img.place(x=470,y=200)
+    
+    R_img_L=Label(window,text="Retieved Image",bg='#1f666b',height=1,width=12,bd=6,font='Helvetica 11 bold')
+    R_img_L.place(x=520,y=160)
     
     Method=combobox.get()                   #Combobox 
     
 
     
 Font_tuple = ("Comic Sans MS", 9,"bold")
-b1= Button(window,text = "Browse Files",
-                        command = browseFiles,bd=4,font=Font_tuple) 
 
-b1.grid(row=0,column=5,pady=30)
-b2=Button(window,text="Submit",bd=4,command=ShowImage,cursor='hand2',font=Font_tuple)
-b2.grid(row=3,column=0,pady=10)
+Browse_B= Button(window,text = "Browse Files",
+                        command = browseFiles,bd=4,cursor='hand2',font=Font_tuple) 
+Browse_B.grid(row=0,column=5,pady=30)
 
-l5=Label(window,text="Choose the retrival method: ",bg='#1f666b',font='Helvetica 11 bold').place(x=5,y=5)
+Submit_B=Button(window,text="Submit",bd=4,command=ShowImage,cursor='hand2',font=Font_tuple)
+Submit_B.grid(row=3,column=0,pady=10)
 
+Exit_B=Button(window,text="Exit",bd=4,command=window.destroy,cursor='hand2',font=Font_tuple)
+Exit_B.grid(row=3,column=5,pady=10)
 
-        
+Choose_L=Label(window,text="Choose the retrival method: ",bg='#1f666b',font='Helvetica 11 bold').place(x=5,y=5)
+
 combobox=ttk.Combobox(window,font='Helvetica 11')
 combobox['values']=('Average RGB','Histogram','Several histograms','CBVR')
 combobox.place(x=210,y=5)
 combobox.current(0)
-
-
-
-b3=Button(window,text="Exit",bd=4,command=window.destroy,font=Font_tuple)
-b3.grid(row=3,column=5,pady=10)
-
 
 window.mainloop()
