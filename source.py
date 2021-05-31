601a7e2ae6b49e43fc9c91296622fe44d7b9c744
Yara Zaki <61282472+yarazakii@users.noreply.github.com>
2021-05-31 23:26:12 +0200

    adding slicing image function

--- source.py
+++ source.py
@@ -2,6 +2,43 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+def sliceImage(img,divisions):
+    # img = cv.imread(Image_path)
+
+    dimensions= img.shape
+    print(dimensions)
+    height = img.shape[0]
+    width = img.shape[1]
+    x=0
+    y=0
+    new_height = height/np.sqrt(divisions)
+    # print(new_height)
+    new_width = width/np.sqrt(divisions)
+    # print(new_width)
+    new_images=[]
+    
+    #image cropping 
+    i=0
+    for i in range(int(np.sqrt(divisions))):
+        y=0
+        for j in range(int(np.sqrt(divisions))):   
+            image= img[ int(y): int(new_height+y), int(x) :int(new_width+x) , : ]
+            y= y + new_height 
+            # print(image)            
+            new_images.append(image)
+            # new_image_path = Image_path[0 : 4]  + str(i) + str(j) + '.jpg'
+            # cv.imwrite(new_image_path, image)
+        
+        x= x+ new_width
+    
+    # image_parts= image_slicer.slice(Image_path, divisions)
+
+    return new_images
+
+
+
 def keyFramesExtracion(cap, threshold):
     if (cap.isOpened()== False):
        print("Error opening video file")
@@ -71,6 +108,14 @@ def keyFramesExtracion(cap, threshold):
     '''
    
     return keyFrames
+
+
+# divisions= 16
+# image= cv.imread("image.jpg")
+# # print(image)
+# slicing= sliceImage(image,divisions)
+# # print(slicing)
+
 
 video_path = 'videos/Lane Detection.mp4'
 threshold = 6
