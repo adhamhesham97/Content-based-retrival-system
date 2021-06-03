fe9f3504a21a4ffbde7fff4ea24698c22051dd6c
Reem-Mady <reemmady@gmail.com>
2021-06-03 04:49:24 +0200

    Add Similarity_Videos,,,Update CompareHistogram,RGB_MEAN

--- source.py
+++ source.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import DB
 import matplotlib.pyplot as plt
 
 def sliceImage(img,divisions):
@@ -43,73 +44,88 @@ slicing= sliceImage(image,divisions)
 # print(slicing)
 '''
 
-def CompareHist(image1, image2, methodtype="Correlation"):
-    image1_r = cv2.resize(image1, (1024, 540),
-               interpolation = cv2.INTER_NEAREST)
-    image2_r = cv2.resize(image2, (1024, 540),
-               interpolation = cv2.INTER_NEAREST)
-################# Method1: Using the OpenCV cv2.compareHist function ###################
-# cv2.compareHist(H1, H2, method)
+# def CompareHist(image1, image2, methodtype="Correlation"):
+#     image1_r = cv2.resize(image1, (1024, 540),
+#                interpolation = cv2.INTER_NEAREST)
+#     image2_r = cv2.resize(image2, (1024, 540),
+#                interpolation = cv2.INTER_NEAREST)
+# ################# Method1: Using the OpenCV cv2.compareHist function ###################
+# # cv2.compareHist(H1, H2, method)
 
-# The cv2.compareHist function takes three arguments: 
-#     H1, which is the first histogram to be compared, 
-#     H2, the second histogram to be compared, 
-#     and method, which is a flag indicating which comparison method should be performed.
+# # The cv2.compareHist function takes three arguments: 
+# #     H1, which is the first histogram to be compared, 
+# #     H2, the second histogram to be compared, 
+# #     and method, which is a flag indicating which comparison method should be performed.
 
-# initialize OpenCV methods for histogram comparison
-    OPENCV_METHODS = (
-    	("Correlation", cv2.HISTCMP_CORREL),
-    	("Chi-Squared", cv2.HISTCMP_CHISQR),
-    	("Intersection", cv2.HISTCMP_INTERSECT),
-    	("Hellinger", cv2.HISTCMP_BHATTACHARYYA))
+# # initialize OpenCV methods for histogram comparison
+#     OPENCV_METHODS = (
+#     	("Correlation", cv2.HISTCMP_CORREL),
+#     	("Chi-Squared", cv2.HISTCMP_CHISQR),
+#     	("Intersection", cv2.HISTCMP_INTERSECT),
+#     	("Hellinger", cv2.HISTCMP_BHATTACHARYYA))
     
     
-    # We start by initializing a reverse variable to False.
-    #  This variable handles how sorting the results dictionary will be performed. 
-    #  For some similarity functions a LARGER value indicates higher similarity 
-    #          (Correlation and Intersection).
-    #  And for others, a SMALLER value indicates higher similarity 
-    #          (Chi-Squared and Hellinger).
+#     # We start by initializing a reverse variable to False.
+#     #  This variable handles how sorting the results dictionary will be performed. 
+#     #  For some similarity functions a LARGER value indicates higher similarity 
+#     #          (Correlation and Intersection).
+#     #  And for others, a SMALLER value indicates higher similarity 
+#     #          (Chi-Squared and Hellinger).
     
-    # loop over the comparison methods
-    for (methodName, method) in OPENCV_METHODS:
-    	# initialize the results dictionary and the sort
-    	# direction
-    	results = {}
-    	reverse = False
+#     # loop over the comparison methods
+#     for (methodName, method) in OPENCV_METHODS:
+#     	# initialize the results dictionary and the sort
+#     	# direction
+#     	results = {}
+#     	reverse = False
         
-        # if we are using the correlation or intersection
-    	# method, then sort the results in reverse order
-    	if methodName in ("Correlation", "Intersection"):
-    		reverse = True
+#         # if we are using the correlation or intersection
+#     	# method, then sort the results in reverse order
+#     	if methodName in ("Correlation", "Intersection"):
+#     		reverse = True
             
-    if methodtype == "Correlation":
-        d = cv2.compareHist(image1_r, image2_r, OPENCV_METHODS[0][1])
-        #print(d)
-    elif methodtype == "Chi-Squared":
-        d = cv2.compareHist(image1_r, image2_r, OPENCV_METHODS[1][1])
-        #print(d)
-    elif methodtype == "Intersection":        
-        d = cv2.compareHist(image1_r, image2_r, OPENCV_METHODS[2][1])
-        #print(d)
-    elif methodtype == "Hellinger":
-        d = cv2.compareHist(image1_r, image2_r, OPENCV_METHODS[3][1])
-        #print(d)
+#     if methodtype == "Correlation":
+#         d = cv2.compareHist(image1_r, image2_r, OPENCV_METHODS[0][1])
+#         #print(d)
+#     elif methodtype == "Chi-Squared":
+#         d = cv2.compareHist(image1_r, image2_r, OPENCV_METHODS[1][1])
+#         #print(d)
+#     elif methodtype == "Intersection":        
+#         d = cv2.compareHist(image1_r, image2_r, OPENCV_METHODS[2][1])
+#         #print(d)
+#     elif methodtype == "Hellinger":
+#         d = cv2.compareHist(image1_r, image2_r, OPENCV_METHODS[3][1])
+#         #print(d)
 
-    if (methodtype == "Correlation") or (methodtype == "Intersection"):
-        if d > 1000000:
-            print("Similar")
-        else:
-            print("Not Similar")
-    elif (methodtype ==  "Chi-Squared") or (methodtype == "Hellinger"):
-        if d < 100000:
-            print("Similar")
-        else:
-            print("Not Similar")
+#     if (methodtype == "Correlation") or (methodtype == "Intersection"):
+#         if d > 1000000:
+#             print("Similar")
+#         else:
+#             print("Not Similar")
+#     elif (methodtype ==  "Chi-Squared") or (methodtype == "Hellinger"):
+#         if d < 100000:
+#             print("Similar")
+#         else:
+#             print("Not Similar")
 
 '''
 CompareHist(image1,image2,"Intersection")
 '''
+def CompareHist (image1,image2):
+    histo1=Histogram(image1)
+    histo2=Histogram(image2)
+    
+    #mini=0
+
+    mminima=np.sum(np.minimum(histo1,histo2))
+    distance=np.divide(mminima,np.sum(histo2))
+    
+    #print(distance)
+    if distance<0.1:
+        return 1
+    else:
+        return 0
+    
 
 def RGB_MEAN(image):
     avg_color_per_row = np.average(image, axis=0)
@@ -127,9 +143,11 @@ def Compare_avg_RGB(image1, image2):
     avg2=RGB_MEAN(image2)
     #print(avg2)
     if abs(avg2[0]-avg1[0]) < 0.1 and abs(avg2[1]-avg1[1]) < 0.1 and abs(avg2[2]-avg1[2]) < 0.1:
-        print("Similar")
+        #print("Similar")
+        return 1
     else:
-        print("NOT Similar")
+        #print("NOT Similar")
+        return 0
 
 #Compare_avg_RGB(image1,image2)
 
@@ -233,8 +251,8 @@ def keyframesfeatures(cap,threshold):
     histogram=[]
     layoutHistogram=[]
     for frame in range(keyframes):
-        avgRGB[frame]= Histogram(frame)
-        histogram[frame] = RGB_MEAN(frame)
+        avgRGB[frame]= RGB_MEAN(frame)
+        histogram[frame] = Histogram(frame)
         layoutHistogram[frame]= layoutHistogram(frame)
     
     
@@ -244,7 +262,7 @@ def keyframesfeatures(cap,threshold):
 
 '''Adding histogram to image slices '''
 
-def layoutHistogram(image,divisions):
+def SeveralHistograms(image,divisions):
     image_slices= sliceImage(image, divisions)
     histogram= [] 
     for image in range(image_slices):
@@ -252,5 +270,30 @@ def layoutHistogram(image,divisions):
     
     return histogram 
 
+
+def Similarity_Video(Video_I,Method):           #########Method : 0 for RGB, 1 for Histogram, 2 for Several histograms
+    Keyframes_I=keyFramesExtracion(Video_I)
+    Similar=[]
+    for video in DB.getVideos():    #loop on videos in DB
+        Keyframes_M=keyFramesExtracion(video)
+        for keyframeM in Keyframes_M:   #loop on keyframes of video inDB
+            for keyframeI in Keyframes_I:   #loop on keyframes of input video
+                if (Method==0):
+                    if(Compare_avg_RGB(keyframeI, keyframeM)):
+                        Similar[video]+=1
+                elif (Method==1):
+                    if(CompareHist(keyframeI, keyframeM)):
+                        Similar[video]+=1
+                elif (Method==1):
+                    if(SeveralHistograms(keyframeI, keyframeM)):
+                        Similar[video]+=1
+        Similar[video]/=keyframeM 
+        
+    maxvalue=max(Similar)
+    max_index=Similar.index(maxvalue)
+    
+    
+    
+    return max_index
 
 
